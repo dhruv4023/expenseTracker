214de4a4b71a81dcb9215ee9bfbdffb382823697
dhruv <dhruv20345@gmail.com>
2023-06-30 11:43:13 +0530

    client changed

--- README.md
+++ README.md
@@ -68,3 +68,10 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+# Modules
+npm i @reduxjs/toolkit
+npm i redux-persist
+npm i react-redux
+npm i react-router-dom
--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import React, { useMemo } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { AllRoutes } from "components/AllRoutes";
 
-function App() {
-  return <div>hello</div>;
-}
+const App = () => {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <AllRoutes />
+        </ThemeProvider>
+      </Router>
+    </>
+  );
+};
 
 export default App;
